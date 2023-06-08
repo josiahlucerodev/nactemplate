d8b814eeabd67c820f2d7924036a90d7d0095873
NovoAffectDev <112781372+NovoAffectDev@users.noreply.github.com>
2023-06-07 20:36:33 -0400

    added nacmake option for external dir

---
+++ externals_nacmake.cmake
@@ -0,0 +1,5 @@
+include(externals/nacmake/dependency.cmake)
+include(externals/nacmake/profile.cmake)
+include(externals/nacmake/utils.cmake)
+include(externals/nacmake/warning.cmake)
+include(externals/nacmake/ipo.cmake)
